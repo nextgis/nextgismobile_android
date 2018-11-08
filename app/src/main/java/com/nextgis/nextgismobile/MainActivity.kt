0c031403ce5402493e0a010c73878b15c9ce2795
Stanislav Petryakov <become.glory@yandex.ru>
2018-11-08 17:27:19 +0300

    Add maplib

--- app/src/main/java/com/nextgis/nextgismobile/MainActivity.kt
+++ app/src/main/java/com/nextgis/nextgismobile/MainActivity.kt
@@ -2,19 +2,29 @@ package com.nextgis.nextgismobile
 
 import android.os.Bundle
 import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
-
+import com.nextgis.maplib.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), GestureDelegate {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        API.init(this@MainActivity)
+        val map = API.getMap("main")
+        map?.let {
+            it.setExtentLimits(MIN_X, MIN_Y, MAX_X, MAX_Y)
+            mapView.setMap(it)
+            addOSMTo(it)
+//            it.save()
+        }
+        mapView.registerGestureRecognizers(this)
+        mapView.freeze = false
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -22,7 +32,16 @@ class MainActivity : AppCompatActivity() {
         }
 
         // Example of a call to a native method
-        sample_text.text = stringFromJNI()
+//        sample_text.text = stringFromJNI()
+    }
+
+    private fun addOSMTo(map: MapDocument) {
+        val dataDir = API.getDataDirectory()
+        if(dataDir != null) {
+            val bbox = Envelope(MIN_X, MAX_X, MIN_Y, MAX_Y)
+            val baseMap = dataDir.createTMS("osm.wconn", OSM_URL, 3857, 0, 18, bbox, bbox, 14)
+            map.addLayer("OSM", baseMap!!)
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -48,6 +67,11 @@ class MainActivity : AppCompatActivity() {
     external fun stringFromJNI(): String
 
     companion object {
+        const val OSM_URL = "http://tile.openstreetmap.org/{z}/{x}/{y}.png"
+        const val MAX_X = 20037508.34
+        const val MIN_X = -MAX_X
+        const val MAX_Y = 20037508.34
+        const val MIN_Y = -MAX_Y
 
         // Used to load the 'native-lib' library on application startup.
         init {
